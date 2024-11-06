27745b839a161cb0ed3af481e2b5dbc3cdc0122f
garden-raccoon <grachevs379@gmail.com>
2024-11-06 21:50:45 +0100

    Update service_grpc.pb.go

--- protocols/orders/service_grpc.pb.go
+++ protocols/orders/service_grpc.pb.go
@@ -116,9 +116,9 @@ func RegisterOrderServiceServer(s grpc.ServiceRegistrar, srv OrderServiceServer)
 	// embedded by pointer and is nil.  This will cause panics if an
 	// unimplemented method is ever invoked, so we test this at initialization
 	// time to prevent it from happening at runtime later due to I/O.
-	if t, ok := srv.(interface{ testEmbeddedByValue() }); ok {
-		t.testEmbeddedByValue()
-	}
+	//if t, ok := srv.(interface{ testEmbeddedByValue() }); ok {
+	//	t.testEmbeddedByValue()
+	//}
 	s.RegisterService(&OrderService_ServiceDesc, srv)
 }
 
